5975a9198fc531afd19831d927d05e3d10b78405
Junemoon <484030869@qq.com>
2023-03-04 23:41:05 +0800

    第二次提交

--- SelectScore/src/views/Result.vue
+++ SelectScore/src/views/Result.vue
@@ -13,8 +13,9 @@ import axios from "axios";
 import { useRouter } from "vue-router";
 import { useRoute } from "vue-router";
 import { reactive } from "vue";
+import { parse } from "@vue/compiler-dom";
 const testdata = reactive(data);
-console.log(testdata);
+// console.log(testdata);
 const router = useRouter();
 const route = useRoute();
 const Logout = async () => {
@@ -35,30 +36,33 @@ const Logout = async () => {
     })
     .catch((err) => console.log(err));
 };
-console.log(route.params);
+let list = decodeURIComponent(route.params.List)
+let StdentData = JSON.parse(list)
+console.log(StdentData.data.data[0]);
+console.log(testdata);
+// console.log(data);
 </script>
 
 <template>
   <main>
     <header>
       <div id="chxy">
-        <a href="https://www.chu.edu.cn/" target="blank"
-          ><img src="../../img/chxy.png" alt=""
-        /></a>
+        <a href="https://www.chu.edu.cn/" target="blank"><img src="../../img/chxy.png" alt="" /></a>
       </div>
       <div class="logo">SELECT SCORE</div>
 
-      <h2>${Name}体测成绩</h2>
+      <h2>{{ data.StudentName }}体测成绩</h2>
     </header>
     <div></div>
     <form>
       <h1>Result</h1>
       <ul>
-        <template v-for="(value, key) in testdata">
+        <template v-for="(value, key) in StdentData.data.data[0]">
           <li v-if="value">
             <span v-if="value"> {{ key }}</span>
             <span v-if="value">{{ value }}</span>
-            <span v-if="value > 95">优秀</span>
+            <!-- <span v-if="value">成绩</span> -->
+            <span v-if="value > 95 && value <= 100">优秀</span>
             <span v-if="value >= 80 && value < 95">良好</span>
             <span v-if="value <= 80 && value >= 60">及格</span>
             <span v-if="value < 60">不及格</span>
@@ -66,14 +70,16 @@ console.log(route.params);
         </template>
         <li>
           <span>总分</span>
-          <span></span>
-          <span>100</span>
+          <!-- 后续换成总分 -->
+          <span>{{ data.subject1 }}</span>
+          <span v-if="data.subject1 > 95 && data.subject1 <= 100">优秀</span>
+          <span v-if="data.subject1 >= 80 && data.subject1 < 95">良好</span>
+          <span v-if="data.subject1 <= 80 && data.subject1 >= 60">及格</span>
+          <span v-if="data.subject1 < 60">不及格</span>
         </li>
       </ul>
       <el-backtop :right="20" :bottom="100" />
-      <el-button type="danger" @click="Logout" class="returnHome"
-        >返回</el-button
-      >
+      <el-button type="danger" @click="Logout" class="returnHome">返回</el-button>
     </form>
 
     <footer><span>德学并举 知行合一</span></footer>
@@ -90,12 +96,14 @@ ul {
   padding: 10px;
   box-sizing: border-box;
 }
+
 ul,
 li {
   padding: 0;
   margin: 0;
   list-style: none;
 }
+
 ul li {
   display: flex;
   background: rgba(255, 255, 255, 0.1);
@@ -103,6 +111,7 @@ ul li {
   margin: 5px 0;
   transition: 0.5s;
 }
+
 /* ul li:nth-child(1) {
   background: #fafcff;
   float: left;
@@ -115,23 +124,28 @@ ul li span:nth-child(1) {
   display: inline-block;
   width: 33%;
 }
+
 ul li span:nth-child(2) {
   display: inline-block;
   width: 67%;
   text-align: right;
 }
+
 ul li span:nth-child(3) {
   display: inline-block;
   width: 33%;
   text-align: right;
 }
+
 ul li:hover {
   transform: scale(1.06);
   background: #f3f4f6;
 }
+
 .ml-4 {
   margin-top: 50px;
 }
+
 main {
   display: flex;
   flex-direction: column;
@@ -141,16 +155,19 @@ main {
   background-color: #e11d48;
   color: #fff;
 }
+
 header {
   padding: 1.5rem;
   width: 100%;
 }
+
 #chxy {
   display: flex;
   justify-content: center;
   align-items: center;
   margin-bottom: 15px;
 }
+
 footer {
   background-color: #fff;
   width: 100%;
@@ -165,7 +182,7 @@ h2 {
   margin-bottom: 1rem;
 }
 
-h2 ~ p {
+h2~p {
   font-weight: 500;
   font-size: 1rem;
 }
@@ -180,10 +197,12 @@ form {
   padding: 3rem 1.5rem 1.5rem 1.5rem;
   width: 100%;
 }
+
 label {
   display: block;
   margin-bottom: 1.5rem;
 }
+
 label span {
   display: block;
   color: #9ca3af;
@@ -191,6 +210,7 @@ label span {
   font-weight: 500;
   margin-bottom: 0.5rem;
 }
+
 input:not([type="submit"]) {
   display: block;
   width: 100%;
@@ -206,6 +226,7 @@ input:not([type="submit"])::placeholder {
   color: #9ca3af;
   font-style: italic;
 }
+
 input[type="submit"] {
   display: block;
   width: fit-content;
@@ -219,25 +240,31 @@ input[type="submit"] {
   cursor: pointer;
   transition: 0.2s ease;
 }
+
 input[type="submit"]:hover {
   background-color: #c32345;
 }
+
 /deep/ input::-webkit-outer-spin-button,
 /deep/ input::-webkit-inner-spin-button {
   -webkit-appearance: none !important;
 }
+
 /deep/ input[type="number"] {
   -moz-appearance: textfield;
 }
+
 .returnHome {
   margin-top: 20px;
   float: right;
 }
+
 footer span {
   font-family: STKaiti;
   font-size: 2.5rem;
   color: #fff;
 }
+
 footer {
   background-color: #e11d48;
 }
